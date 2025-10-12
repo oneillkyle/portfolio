23ba68f3fdecdfaf29ede53a7ea9c13c26a69f4f
Kyle O'Neill <oneillkyle@gmail.com>
2025-10-12 16:22:23 -0700

    removed old code from the ai project

--- ai/models/export_model.py
+++
@@ -1,6 +0,0 @@
-
-import tensorflow as tf
-
-model = tf.keras.models.load_model("ai/saved_models/trained_nq_model.keras")
-model.save("export_hf_format", save_format="tf")
-print("✅ Saved model to export_hf_format/")
